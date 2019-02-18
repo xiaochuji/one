2fa57980956267ef38ffbc39f4973f98fcfff02f
zl <1158464239@qq.com>
2019-02-18 09:24:55 +0800

    排版及压缩sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,13 @@ var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 var clean = require("gulp-clean-css");
 var uglify = require("gulp-uglify");
-var babel = require("gulp-babel");
 var server = require("gulp-webserver");
 
 //编译sass
 gulp.task("sass",function(){
     return gulp.src("./src/scss/*.scss")
            .pipe(sass())
-           .pipe("./src/css")
+           .pipe(gulp.dest("./src/css/"))
 })
 
 //编译js
@@ -24,22 +23,22 @@ gulp.task("ug",function(){
 //压缩css
 gulp.task("concatCss",function(){
     return gulp.src("./src/css/*.css")
-           .pipe(concat(all.css))
+           .pipe(concat("all.css"))
            .pipe(clean())
            .pipe(gulp.dest("./src/css"))
 })
 
 //压缩js
-gulp.task("concatjs",function(){
-    return gulp.src("./src/js/*.js")
-           .pipe(concat("all.js"))
-           .pipe(gulp.dest("./src/js"))
-})
+// gulp.task("concatjs",function(){
+//     return gulp.src("./src/js/*.js")
+//            .pipe(concat("all.js"))
+//            .pipe(gulp.dest("./src/js"))
+// })
 
 //监听
 gulp.task("watch",function(){
     gulp.watch("./src/scss/*.scss",gulp.series("sass","concatCss"))
-    gulp.watch("./src/libs/*.js",gulp.series("ug","caoncatjs"))
+    // gulp.watch("./src/libs/*.js",gulp.series("ug","caoncatjs"))
 })
 
 //起服务
